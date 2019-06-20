869f475ed881b84c2a8fc76fb745fef4865eaa5b
VishalSharma0309 <vishals7654@gmail.com>
2019-06-20 17:11:07 +0530

    installation script

--- install_libraries_arduinoIDE/install.sh
+++ install_libraries_arduinoIDE/install.sh
@@ -1,1 +1,15 @@
 # Installation script for all the libraries mentioned in the repository
+
+# Made by Vishal Sharma (https://github.com/VishalSharma0309)
+
+#! /bin/bash
+# script to install ROS-melodic directly into a linux system
+# tested on Ubuntu 18.04.02
+
+#use sudo to run this command
+if [ "$(id -u)" -ne 0 ]
+        then echo "Must be root"
+        exit
+fi
+
+
